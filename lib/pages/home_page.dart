53c8ddadd974bc0c4b5cd9db755bf0a6aa5d6661
Estudiantes UPB <estudiante.upb@bga.upb>
2025-09-06 10:25:16 -0500

    commit taller 2

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,8 +16,8 @@ class _HomePageState extends State<HomePage> {
   List<UserModel> users = [
     UserModel(name: "Emma Garcia", age: 17, category: "Menor de Edad", image:"https://i.pinimg.com/474x/cf/ef/0c/cfef0cb6580ded769722a082693e1b09.jpg", email: "emma@gmail.com",
       phone: "3001234567", address: "Cali, Colombia", bio: "Estudiante apasionada por la música y la lectura."),
-    UserModel (name: "Rafael Alfonso Senegal Florez", age: 36, category: "Mayor de Edad", image:"https://i.imgur.com/f9DWaZc.jpeg", email: "rafael@gmail.com",
-      phone: "3109876543", address: "Bogotá, Colombia", bio: "Ingeniero electrónico con interés en deportes extremos."),
+    UserModel (name: "Luis Alfredo Rodriguez Jaimes", age: 29, category: "Mayor de Edad", image:"https://img.freepik.com/vector-gratis/circulo-azul-usuario-blanco_78370-4707.jpg", email: "luisrodriguezj96@gmail.com",
+      phone: "3208248533", address: "Piedecuesta, Santander, Colombia", bio: "Eso mismo me pregunto, me gusta el fútbol."),
     UserModel (name: "Gabriel Fernandez", age: 26, category: "Mayor de Edad", image:"https://i.pinimg.com/736x/00/f9/26/00f9261acd6be2b9b1e530e27fc942ac.jpg", email: "gabriel@gmail.com",
       phone: "3151112233", address: "Medellín, Colombia", bio: "Diseñador gráfico que ama el arte digital y la fotografía."),
     UserModel (name: "Isabella Palacios", age: 22, category: "Mayor de Edad", image:"https://us.123rf.com/450wm/andreypopov/andreypopov1209/andreypopov120900870/15512137-portrait-of-a-beautiful-brunette-woman-in-profile-with-downcast-eyes-and-a-serious-expression.jpg", email: "isabella@gmail.com",
